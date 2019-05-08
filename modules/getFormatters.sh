843f77b1a15564536770ddd8d96b1047a5294354
APixel Visuals <apixelvisualsofficial@gmail.com>
2019-05-08 07:38:14 -0700

    Added some more colors

--- modules/getFormatters.sh
+++ modules/getFormatters.sh
@@ -7,22 +7,25 @@ function GET_FORMATTERS() {
     case $PRIMARY_LANGUAGE in
 
         # Red
-        "HTML") COLOR='\033[0;31m';;
+        "HTML" | "Rust" | "Ruby") COLOR='\033[0;31m';;
 
         # Yellow
-        "Java") COLOR='\033[0;33m';;
+        "Java" | "Swift") COLOR='\033[0;33m';;
 
         # Blue
-        "CSS" | "TypeScript" | "Python" | "C++") COLOR='\033[0;34m';;
+        "CSS" | "TypeScript" | "Python" | "C++" | "PHP" | "JSONiq" | "CoffeeScript" | "Lua" | "Perl" | "R") COLOR='\033[0;34m';;
 
         # Magenta
         "C#" | "TypeScript") COLOR='\033[0;35m';;
 
         # Light Gray
-        "C") COLOR='\033[0;37m';;
+        "C" | "Shell" | "PowerShell" | "Markdown") COLOR='\033[0;37m';;
 
         # Bright yellow
         "JavaScript") COLOR='\033[0;93m';;
+
+        # Bright blue
+        "Dockerfile") COLOR='\033[0;94m';;
 
         # Other (bright yellow)
         *) COLOR='\033[0;93m';;
