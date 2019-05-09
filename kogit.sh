3701c63afd27883fa0e723331eedcde8867127b4
APixel Visuals <apixelvisualsofficial@gmail.com>
2019-05-09 09:16:24 -0700

    Renamed `modules/` directory to `kogit-modules`

--- kogit.sh
+++ kogit.sh
@@ -13,7 +13,7 @@ CONFIG=$(cat ~/.config/kogit/config.json)
 ACCESS_TOKEN=$(echo "$CONFIG" | jq -r ".accessToken")
 
 # Set token
-source "$(dirname "$0")/modules/setToken.sh"
+source "$(dirname "$0")/kogit-modules/setToken.sh"
 if [ "$1" == "-t" ] || [ "$1" == "--token" ]; then SET_TOKEN $2; fi
 
 # No access token
@@ -25,36 +25,36 @@ if [ "$ACCESS_TOKEN" == "null" ]; then
 fi
 
 # Fetch data
-source "$(dirname "$0")/modules/fetchData.sh"
+source "$(dirname "$0")/kogit-modules/fetchData.sh"
 FETCH_DATA $1
 
 # Clear screen
 clear
 
 # Get formatters
-source "$(dirname "$0")/modules/getFormatters.sh"
+source "$(dirname "$0")/kogit-modules/getFormatters.sh"
 GET_FORMATTERS
 
 # Title
-source "$(dirname "$0")/modules/printers/title.sh"
+source "$(dirname "$0")/kogit-modules/printers/title.sh"
 PRINT_TITLE
 
 # Description
-source "$(dirname "$0")/modules/printers/description.sh"
+source "$(dirname "$0")/kogit-modules/printers/description.sh"
 PRINT_DESCRIPTION
 
 # Website
-source "$(dirname "$0")/modules/printers/website.sh"
+source "$(dirname "$0")/kogit-modules/printers/website.sh"
 PRINT_WEBSITE
 
 # Stats
-source "$(dirname "$0")/modules/printers/stats.sh"
+source "$(dirname "$0")/kogit-modules/printers/stats.sh"
 PRINT_STATS
 
 # Details
-source "$(dirname "$0")/modules/printers/details.sh"
+source "$(dirname "$0")/kogit-modules/printers/details.sh"
 PRINT_DETAILS
 
 # Primary language
-source "$(dirname "$0")/modules/printers/primaryLanguage.sh"
+source "$(dirname "$0")/kogit-modules/printers/primaryLanguage.sh"
 PRINT_PRIMARY_LANGUAGE
