5a42af80c4b6b7e1e0c7f1abefe6fbafe075fba6
APixel Visuals <apixelvisualsofficial@gmail.com>
2019-05-09 09:14:40 -0700

    Made all `source` commands use absolute path

--- kogit.sh
+++ kogit.sh
@@ -13,7 +13,7 @@ CONFIG=$(cat ~/.config/kogit/config.json)
 ACCESS_TOKEN=$(echo "$CONFIG" | jq -r ".accessToken")
 
 # Set token
-source ./modules/setToken.sh
+source "$(dirname "$0")/modules/setToken.sh"
 if [ "$1" == "-t" ] || [ "$1" == "--token" ]; then SET_TOKEN $2; fi
 
 # No access token
@@ -25,36 +25,36 @@ if [ "$ACCESS_TOKEN" == "null" ]; then
 fi
 
 # Fetch data
-source ./modules/fetchData.sh
+source "$(dirname "$0")/modules/fetchData.sh"
 FETCH_DATA $1
 
 # Clear screen
 clear
 
 # Get formatters
-source ./modules/getFormatters.sh
+source "$(dirname "$0")/modules/getFormatters.sh"
 GET_FORMATTERS
 
 # Title
-source ./modules/printers/title.sh
+source "$(dirname "$0")/modules/printers/title.sh"
 PRINT_TITLE
 
 # Description
-source ./modules/printers/description.sh
+source "$(dirname "$0")/modules/printers/description.sh"
 PRINT_DESCRIPTION
 
 # Website
-source ./modules/printers/website.sh
+source "$(dirname "$0")/modules/printers/website.sh"
 PRINT_WEBSITE
 
 # Stats
-source ./modules/printers/stats.sh
+source "$(dirname "$0")/modules/printers/stats.sh"
 PRINT_STATS
 
 # Details
-source ./modules/printers/details.sh
+source "$(dirname "$0")/modules/printers/details.sh"
 PRINT_DETAILS
 
 # Primary language
-source ./modules/printers/primaryLanguage.sh
+source "$(dirname "$0")/modules/printers/primaryLanguage.sh"
 PRINT_PRIMARY_LANGUAGE
