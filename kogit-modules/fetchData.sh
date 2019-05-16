7bbb83446a085eaaee75ff4366ae94d1134c851c
APixel Visuals <apixelvisualsofficial@gmail.com>
2019-05-16 07:13:38 -0700

    Rate limit error now tells you how long you need to wait before making another request

--- kogit-modules/fetchData.sh
+++ kogit-modules/fetchData.sh
@@ -24,8 +24,18 @@ function FETCH_DATA() {
     # Rate limited
     if [[ "$ERROR" == "API rate limit exceeded"* ]]; then
 
-        if [ "$ACCESS_TOKEN" == "null" ]; then echo "You have hit the rate limit, however, you can set a GitHub access token in order to have a better rate limit by using \`kogit -t ACCESS_TOKEN\`"
-        else echo "You have hit the rate limit"; fi
+        # Get time left
+        if [ "$ACCESS_TOKEN" == "null" ]; then TIME_LEFT=$(curl -s https://api.github.com/rate_limit)
+        else TIME_LEFT=$(curl -sH "Authorization: token $ACCESS_TOKEN" https://api.github.com/rate_limit); fi
+
+        # Parse time left
+        TIME_LEFT=$(( $(echo "$TIME_LEFT" | jq -r ".rate.reset") - $(date +%s) ))
+        MINUTES_LEFT=$(( $TIME_LEFT / 60 ))
+        SECONDS_LEFT=$(( $TIME_LEFT % 60 ))
+
+        # Send error
+        if [ "$ACCESS_TOKEN" == "null" ]; then echo "You have hit the rate limit, however, you can set a GitHub access token in order to have a better rate limit by using \`kogit -t ACCESS_TOKEN\`. Otherwise, please wait another $MINUTES_LEFT minutes and $SECONDS_LEFT seconds"
+        else echo "You have hit the rate limit. Please wait another $MINUTES_LEFT minutes and $SECONDS_LEFT seconds"; fi
         exit
 
     fi
